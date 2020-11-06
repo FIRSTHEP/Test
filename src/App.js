9650d8a6c9b8eab27395683af37b6c437907efd0
FIRST <first@FIRSTHEPs-MacBook-Pro-2019.local>
2020-11-06 07:22:44 +0700

    Daily commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
-import mockData from '../src/mockData'
-import mockTableData from '../src/mockTableData'
 import styled from 'styled-components'
 import ThaiIcon from '../src/Assets/thailand.png'
 import UsIcon from '../src/Assets/united-states.png'
 import LaosIcon from '../src/Assets/laos.png'
 import {
+  Modal,
   Card,
   Form,
   Input,
@@ -15,54 +14,91 @@ import {
   Select,
   Row,
   Col,
-  Checkbox,
   Button,
   DatePicker,
-  message,
   Divider,
-  Table
+  Table,
+  Spin
 } from 'antd';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+
 const App = () => {
   const [data, setData] = useState([])
+  const [selectedRowKeys, setSelectedRowKeys] = useState([])
+  const [visible, setVisible] = useState(false);
+  const [title, setTitle] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [gender, setGender] = useState('')
+  const [mobilePhone, setMobilePhone] = useState('')
+  const [prefix, setPrefix] = useState('')
+  const [nationality, setNationality] = useState('')
+  const [taskId, setTaskId] = useState('')
+
+  const dateFormat = 'DD/MM/YYYY';
   const [form] = Form.useForm();
   const { Option } = Select;
-  const [selectedRowKeys, setSelectedRowKeys] = useState([])
-  const dateFormat = 'DD/MM/YYYY';
 
   useEffect(() => {
-    localStorage.setItem('user-data', JSON.stringify(data))
+    const localData = localStorage.getItem('local-data')
+    console.log('localStorage:', JSON.parse(localData))
+    if (localData !== null) {
+      setData(JSON.parse(localData))
+    }
   }, [])
 
   const onFinish = (values) => {
-    setData([...data, values])
+    localStorage.clear();
+    let list = [...data, values]
+    const newData = list.map((x, i) => {
+      x.id = i + 1
+      return x
+    })
+    setData(newData)
+    localStorage.setItem('local-data', JSON.stringify(newData))
+    window.location.reload()
   }
 
   const columns = [
+    {
+      width: 100,
+      title: 'ID',
+      dataIndex: 'id',
+      render: (text, record) => (
+        <span>{record.id ? record.id : '-'}</span>
+      ),
+    },
     {
       width: 300,
       title: 'Name',
       dataIndex: 'firstName',
       render: (text, record) => (
-        <>
-          <span>{record.firstName}{' '}{record.lastName}</span>
-        </>
+        <span>{record.firstName}{' '}{record.lastName}</span>
       ),
     },
     {
       width: 300,
       title: 'Gender',
       dataIndex: 'gender',
+      render: (text, record) => (
+        <span>{record.gender ? record.gender : '-'}</span>
+      ),
     },
     {
       width: 300,
       title: 'Mobile Phone',
       dataIndex: 'mobilePhone',
+      render: (text, record) => (
+        <>{record.prefix && record.mobilePhone ? <span>{record.prefix + record.mobilePhone}</span> : '-'}</>
+      ),
     },
     {
       width: 300,
       title: 'Nationality',
       dataIndex: 'nationality',
+      render: (text, record) => (
+        <span>{record.nationality ? record.nationality : '-'}</span>
+      ),
     },
     {
       width: 300,
@@ -73,55 +109,214 @@ const App = () => {
           <Button
             icon={(<EditOutlined />)}
             style={{ width: '48px', height: '48px', marginRight: '0.5em' }}
-            onClick={() => { }}
-          />
+            onClick={() => { showEditModal(record.id) }} />
           <Button
             icon={(<DeleteOutlined />)}
             style={{ width: '48px', height: '48px', marginLeft: '0.5em' }}
-            onClick={() => { }}
-          />
+            onClick={() => { confirmRemove(record.id) }} />
         </>
       ),
     },
   ];
 
-  const prefixSelector = (
-    <Form.Item name="prefix" noStyle>
-      <Select style={{ width: 100 }} defaultValue="66">
-        <Option value="66"><div><img src={ThaiIcon} alt="thai_icon" width="14" height="auto"/>{' '}<span>+66</span></div></Option>
-        <Option value="1"><div><img src={UsIcon} alt="us_icon" width="14" height="auto"/>{' '}<span>+1</span></div></Option>
-        <Option value="856"><div><img src={LaosIcon} alt="laos_icon" width="14" height="auto"/>{' '}<span>+856</span></div></Option>
-      </Select>
-    </Form.Item>
-  );
+  const confirmEdit = () => {
+    const { confirm } = Modal
+    confirm({
+      title: 'Confirm Edit',
+      content: 'Are you sure to edit this task?',
+      okText: 'Yes',
+      okButtonProps: {
+        type: 'danger',
+        text: 'Yes',
+        style: { height: '40px', width: '65px' },
+      },
+      cancelButtonProps: {
+        style: { height: '40px', width: '65px' },
+      },
+      cancelText: 'No',
+      onOk() {
+        editTask()
+      },
+    })
+  }
+
+  const editTask = () => {
+    localStorage.clear();
+    const newData = data.map(i =>
+      i.id === taskId ? {
+        ...i,
+        title: title,
+        firstName: firstName,
+        lastName: lastName,
+        gender: gender,
+        prefix: prefix,
+        mobilePhone: mobilePhone,
+        nationality: nationality
+      } : i
+    );
+    setData(newData)
+    localStorage.setItem('local-data', JSON.stringify(newData))
+    cancelEditModal()
+    window.location.reload()
+  }
+
+  const confirmRemove = (taskId) => {
+    const { confirm } = Modal
+    confirm({
+      centered: true,
+      title: `Confirm Remove Task ID : ${taskId}`,
+      content: 'Are you sure remove this task?',
+      okText: 'Yes',
+      okButtonProps: {
+        type: 'danger',
+        text: 'Yes',
+        style: { height: '40px', width: '65px' },
+      },
+      cancelButtonProps: {
+        style: { height: '40px', width: '65px' },
+      },
+      cancelText: 'No',
+      onOk() {
+        removeTask(taskId)
+      },
+    })
+  }
+
+  const removeRowSelected = () => {
+    const newData = setSelectedRowKeys.filter((i) => { return i.id })
+    console.log(newData)
+  }
+
+
+  const removeTask = (taskId) => {
+    localStorage.clear();
+    const newData = data.filter((i) => {
+      return i.id !== taskId
+    })
+    setData(newData)
+    localStorage.setItem('local-data', JSON.stringify(newData))
+    window.location.reload()
+  }
 
   const rowSelection = {
     onChange: (selectedRowKeys, selectedRows) => {
       setSelectedRowKeys(selectedRows)
-      console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+      // console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
     },
   };
 
-  const hasSelected = selectedRowKeys.length > 0;
-  const hasData = data.length > 0;
+  const showEditModal = (taskId) => {
+    setTaskId(taskId)
+    setVisible(true);
+  };
 
+  const cancelEditModal = () => {
+    setVisible(false);
+  };
+
+  const hasSelected = selectedRowKeys.length > 0;
+  const hasData = data !== null && data.length > 0;
   return (
     <>
+      <Modal
+        title={`Edit Task ID : ${taskId}`}
+        visible={visible}
+        onOk={() => { confirmEdit() }}
+        onCancel={cancelEditModal}>
+        <Row gutter={[16, 16]}>
+          <Col span={24} >
+            Title:
+              <Select
+              style={{ width: '100%' }}
+              onSelect={(e) => { setTitle(e) }}
+              placeholder="-- Please Select --">
+              <Option value='Mr.'>Mr.</Option>
+              <Option value='Mrs.'>Mrs.</Option>
+              <Option value='Miss'>Miss</Option>
+              <Option value='Ms.'>Ms.</Option>
+            </Select>
+          </Col>
+        </Row>
+        <Row gutter={[16, 16]}>
+          <Col span={12} >
+            First Name:
+              <Input
+              onChange={(e) => { setFirstName(e.target.value) }}
+              type="text"
+              block="true"
+              allowClear
+            />
+          </Col>
+          <Col span={12} >
+            Last Name:
+              <Input
+              onChange={(e) => { setLastName(e.target.value) }}
+              type="text"
+              block="true"
+              allowClear
+            />
+          </Col>
+        </Row>
+        <Row gutter={[16, 16]}>
+          <Col span={24} >
+            Nationality:
+              <Select
+              onSelect={(e) => { setNationality(e) }}
+              placeholder="-- Please Select --" style={{ width: '100%' }}>
+              <Option value='THAI'>THAI</Option>
+              <Option value='AMERICAN'>AMERICAN</Option>
+              <Option value='LAOS'>LAOS</Option>
+            </Select>
+          </Col>
+        </Row>
+        <Row gutter={[16, 16]}>
+          <Col span={24} >
+            Gender:
+              <Radio.Group
+              onChange={(e) => { setGender(e.target.value) }}
+              style={{ paddingLeft: '1rem' }}>
+              <Radio value="Male">Male</Radio>
+              <Radio value="Female">Female</Radio>
+              <Radio value="Unisex">Unisex</Radio>
+            </Radio.Group>
+          </Col>
+        </Row>
+        <Row gutter={[16, 16]}>
+          <Col span={14} >
+            Mobile Phone:
+              <Input
+              onChange={(e) => { setMobilePhone(e.target.value) }}
+              type="number"
+              allowClear />
+          </Col>
+          <Col span={10} >
+            Country Code:
+              <Select
+              onChange={(e) => { setPrefix(e) }}
+              placeholder="-- Please Select --"
+              style={{ width: '100%' }}>
+              <Option value="+66"><div><img src={ThaiIcon} alt="thai_icon" width="14" height="auto" />{' '}<span>+66</span></div></Option>
+              <Option value="+1"><div><img src={UsIcon} alt="us_icon" width="14" height="auto" />{' '}<span>+1</span></div></Option>
+              <Option value="+856"><div><img src={LaosIcon} alt="laos_icon" width="14" height="auto" />{' '}<span>+856</span></div></Option>
+            </Select>
+          </Col>
+        </Row>
+      </Modal>
       <div style={{ margin: '2rem' }}>
         <CardStyled>
           <Form
             form={form}
             onFinish={onFinish}
             autoComplete="off"
-            initialValues={mockData.data}>
-
+            initialValues={data}>
             <Row gutter={[16, 4]}>
-              <Col span={4}>
+              <Col span={6}>
                 <Form.Item
                   name="title"
                   label={(<strong>Title</strong>)}
-                  rules={[{ required: true, message: 'Title is required' }]}>
-                  <Select>
+                // rules={[{ required: true, message: 'Title is required' }]}
+                >
+                  <Select placeholder="-- Please Select --">
                     <Option value='Mr.'>Mr.</Option>
                     <Option value='Mrs.'>Mrs.</Option>
                     <Option value='Miss'>Miss</Option>
@@ -129,11 +324,12 @@ const App = () => {
                   </Select>
                 </Form.Item>
               </Col>
-              <Col span={10}>
+              <Col span={9}>
                 <Form.Item
                   name="firstName"
                   label={(<strong>First Name</strong>)}
-                  rules={[{ required: true, message: 'First Nameis required' }]}>
+                // rules={[{ required: true, message: 'First Name required' }]}
+                >
                   <Input
                     type="text"
                     block="true"
@@ -141,11 +337,12 @@ const App = () => {
                   />
                 </Form.Item>
               </Col>
-              <Col span={10}>
+              <Col span={9}>
                 <Form.Item
                   name="lastName"
                   label={(<strong>Last Name</strong>)}
-                  rules={[{ required: true, message: 'Last Name is required' }]}>
+                // rules={[{ required: true, message: 'Last Name is required' }]}
+                >
                   <Input
                     type="text"
                     block="true"
@@ -154,21 +351,23 @@ const App = () => {
                 </Form.Item>
               </Col>
             </Row>
-
             <Row gutter={[16, 4]}>
               <Col span={12}>
                 <Form.Item
                   label={(<strong>Birthdate</strong>)}
                   name="birthdate"
-                  rules={[{ required: true, message: 'Birthdate is required' }]}>
+                // rules={[{ required: true, message: 'Birthdate is required' }]}
+                >
                   <DatePicker style={{ width: '100%' }} format={dateFormat} />
                 </Form.Item>
               </Col>
               <Col span={12}>
                 <Form.Item
                   name="nationality"
-                  label={(<strong>Nationality</strong>)}>
-                  <Select>
+                  label={(<strong>Nationality</strong>)}
+                // rules={[{ required: true, message: 'Nationality is required' }]}
+                >
+                  <Select placeholder="-- Please Select --">
                     <Option value='THAI'>THAI</Option>
                     <Option value='AMERICAN'>AMERICAN</Option>
                     <Option value='LAOS'>LAOS</Option>
@@ -176,13 +375,13 @@ const App = () => {
                 </Form.Item>
               </Col>
             </Row>
-
             <Row gutter={[16, 4]}>
               <Col span={16}>
                 <Form.Item
-                  style={{ paddingLeft: '.5rem' }}
                   name="citizen"
-                  label={(<strong>Citizen ID</strong>)}>
+                  label={(<strong>Citizen ID</strong>)}
+                // rules={[{ required: true, message: 'Citizen ID is required' }]}
+                >
                   <Input
                     maxLength={13}
                     type="number"
@@ -191,60 +390,69 @@ const App = () => {
                 </Form.Item>
               </Col>
             </Row>
-
             <Row gutter={[16, 4]}>
               <Col>
                 <Form.Item
-                  style={{ paddingLeft: '.5rem' }}
                   name="gender"
-                  label={(<strong>Gender</strong>)}>
+                  label={(<strong>Gender</strong>)}
+                // rules={[{ required: true, message: 'Gender is required' }]}
+                >
                   <Radio.Group style={{ paddingLeft: '1rem' }}>
-                    <Radio style={{ width: 150}} value="male">Male</Radio>
-                    <Radio style={{ width: 150}}  value="female">Female</Radio>
-                    <Radio style={{ width: 150}}  value="unisex">Unisex</Radio>
+                    <Radio style={{ width: 150 }} value="Male">Male</Radio>
+                    <Radio style={{ width: 150 }} value="Female">Female</Radio>
+                    <Radio style={{ width: 150 }} value="Unisex">Unisex</Radio>
                   </Radio.Group>
                 </Form.Item>
               </Col>
             </Row>
-
             <Row gutter={[16, 4]}>
-              <Col span={10}>
+              <Col span={12}>
                 <Form.Item name="mobilePhone"
                   label={(<strong>Mobile Phone</strong>)}
-                  rules={[{ required: true, message: 'Mobile Phone is required' }]}>
-                  <Input
-                    type="number"
-                    addonBefore={prefixSelector}
-                  />
+                // rules={[{ required: true, message: 'Mobile Phone is required' }]}
+                >
+                  <Input type="number" allowClear />
+                </Form.Item>
+              </Col>
+              <Col span={5}>
+                <Form.Item name="prefix"
+                // rules={[{ required: true, message: 'Country Code is required' }]}
+                >
+                  <Select placeholder="-- Country Code --">
+                    <Option value="+66"><div><img src={ThaiIcon} alt="thai_icon" width="14" height="auto" />{' '}<span>+66</span></div></Option>
+                    <Option value="+1"><div><img src={UsIcon} alt="us_icon" width="14" height="auto" />{' '}<span>+1</span></div></Option>
+                    <Option value="+856"><div><img src={LaosIcon} alt="laos_icon" width="14" height="auto" />{' '}<span>+856</span></div></Option>
+                  </Select>
                 </Form.Item>
               </Col>
             </Row>
-
             <Row gutter={[16, 4]}>
-              <Col span={10}>
+              <Col span={12}>
                 <Form.Item
-                  style={{ paddingLeft: '.5rem' }}
                   name="passportNo"
-                  label={(<strong >Passport No</strong>)}>
+                  label={(<strong >Passport No</strong>)}
+                // rules={[{ required: true, message: 'Passport No is required' }]}
+                >
                   <Input
                     allowClear
                   />
                 </Form.Item>
               </Col>
             </Row>
-
             <Row gutter={[16, 4]}>
-            <Col span={10}>
+              <Col span={12}>
                 <Form.Item
                   name="expectedSalary"
                   label={(<strong>Expected Salary</strong>)}
-                  rules={[{ required: true, message: 'Expected Salary is required' }]}>
+                // rules={[{ required: true, message: 'Expected Salary is required' }]}
+                >
                   <Input
+                    type="number"
                     prefix="฿"
                     suffix="THB" />
                 </Form.Item>
               </Col>
-              <Col span={14}>
+              <Col span={12}>
                 <Form.Item>
                   <Button
                     style={{ width: '50%', float: "right" }}
@@ -257,9 +465,7 @@ const App = () => {
             </Row>
           </Form>
         </CardStyled>
-
-        <Divider style={{ marginTop: '1.5rem' }} />
-
+        <Divider style={{ marginTop: '2rem' }} />
         {hasData &&
           <div style={{
             left: 0,
@@ -271,11 +477,12 @@ const App = () => {
           }}>
             <Button
               type="danger"
-              disabled={!hasSelected}>
+              disabled={!hasSelected}
+              onClick={() => { removeRowSelected() }}
+            >
               Delete
               </Button>
           </div>}
-
         <Table
           rowKey="id"
           columns={columns}
@@ -285,7 +492,6 @@ const App = () => {
           pagination={{ position: ['topRight'] }}>
         </Table>
         <br />
-
       </div>
     </>
   );
